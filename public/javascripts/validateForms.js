e0948acdc9b3a7f28984afd03c123afb6f23d223
Adam <adamwalkerlondon@gmail.com>
2021-05-08 18:36:07 +0100

    Issue solve attempt 2.

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -18,11 +18,9 @@
 })()
 
 //Function to prevent file update button being pressed on editUser page when no file present. 
-/*
 document.getElementById("picSubmit").onclick = function(e) {
   if (document.getElementById("profileImage").value == "") {
     e.preventDefault();
     document.getElementById("noData").classList.remove("hidden");
   }
 }
-*/
